4fb286ff3cebdcfbd407ef7651245a02b179665b
Isa Sierra <siemeris@gmail.com>
2023-12-23 18:48:04 +0100

    Refactor check_arg function and fix formatting issues. Norminette OK

--- check_inputs.c
+++ check_inputs.c
@@ -1,99 +1,86 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   check_inputs.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: issierra <issierra@student.42madrid.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/12/23 18:22:11 by issierra          #+#    #+#             */
+/*   Updated: 2023/12/23 18:47:11 by issierra         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "push_swap.h"
 
-
-//CONTROL DE ERRORES:
-//Si hay algún error, el programa deberá mostrar "Error" seguido de un salto de línea y
-// devolver el control al sistema operativo (return (1)).
-//Los errores pueden ser de dos tipos:
-//• Formato de entrada: los parámetros no son números enteros, hay duplicados, hay
-// algún parámetro que no es un número, etc. 
-// Puede admitir a la vez numeros string y numeros int.
-// Puede admitir negativos.
-
-
-int check_arg(int argc, char **arg)
+static int	chars(char**arg)
 {
-    int i;
-    int j;
-    int len;
+	int	i;
+	int	len;
 
+	i = 1;
+	while (arg[i])
+	{
+		len = ft_strlen(arg[i]);
+		while (len >= 1)
+		{
+			if (len -1 != 0 && (arg[i][len - 1] == 45 || arg[i][len - 1] == 43))
+			{
+				ft_printf("Error\nNúmeros con caracter + o - en medio");
+				return (0);
+			}
+			if (!((arg[i][len - 1] >= 48 && arg[i][len - 1] <= 57)
+				|| (arg[i][len - 1] == 45) || (arg[i][len - 1] == 43)))
+			{
+				ft_printf("Error\nCaracter no válido");
+				return (0);
+			}
+			len--;
+		}
+		i++;
+	}
+	return (1);
+}
 
+static	int	repeated_numbers(int argc, char**arg)
+{
+	int	i;
+	int	j;
 
+	i = 1;
+	while (i < argc - 1)
+	{
+		j = i + 1;
+		while (arg[j])
+		{
+			if (ft_atoi(arg[i]) == ft_atoi(arg[j]))
+			{
+				ft_printf("Error\nNúmeros repes %i", ft_atoi(arg[i]));
+				return (0);
+			}
+			j++;
+		}
+		i++;
+	}
+	return (1);
+}
 
-    //Comprobamos si los argumentos vienen en una sola cadena o en varias
-    // if (argc == 2 && arg[1][1] != '\0')
-    // {
-    //     arg = ft_split(arg[1], ' ');
-    //     //imprimimos la lista de argumentos
-    //     i = 0;
-    //     while(arg[i])
-    //     {
-    //         ft_printf("arg %i %s", i, arg[i]);
-    //         i++;
-    //     }
-    // argc = i;
-    // }
-    
-    i = 1;
-    j = i + 1;
-    
-    while(arg[i])
-    {
-        len = ft_strlen(arg[i]);
-        // ft_printf("len %i", len );
-        while(len >= 1)
-        {
-            if (len -1 != 0 && (arg[i][len-1] == 45 || arg[i][len-1] == 43))
-            {
-                ft_printf("Error\nNúmeros con caracter + o - en medio");
-                return(0);
-            }
-            // ft_printf("caracter %c", arg[i][len-1]);
-            if (!((arg[i][len-1] >= 48 && arg[i][len-1] <= 57) || (arg[i][len-1] == 45) || (arg[i][len-1] == 43)))
-            {
-                ft_printf("Error\nCaracter no válido");
-                return(0);
-            }
-            len--; 
-        }
-        i++;
-    }
+int	check_arg(int argc, char **arg)
+{
+	int	i;
 
-    //Comprobamos que no haya números mayores de 2147483647 o menores de -2147483648
-    i = 1;
-    while (i < argc)
-    {
-        if (ft_atoi(arg[i]) > 2147483647 || ft_atoi(arg[i]) < -2147483648)
-        {
-            ft_printf("Error\nNúmeros fuera de rango");
-            return (0);
-        }
-        i++;
-    }
-
-    //comprobamos que no hay número repetidos
-    i = 1;
-    //ft_printf("i %i atoi %i", i, ft_atoi(arg[j]));
-    while (i < argc - 1)
-    {
-        j = i + 1;
-        while(arg[j])
-        {
-            if (ft_atoi(arg[i])==ft_atoi(arg[j]))
-            {
-                ft_printf("Error\nNúmeros repes %i %i", ft_atoi(arg[i]), ft_atoi(arg[j]));
-                return (0);
-            }
-        //comprobamos que sea numero y que sea menor de 2147483647 y mayor de -2147483648
-         
-            // if (ft_atoi(arg[i]) > 2147483647 || ft_atoi(arg[i]) < -2147483648)
-            // {
-            //     ft_printf("Error\nNúmeros fuera de rango");
-            //     return (0);
-            // }
-            j++;
-        }
-        i++;
-    }
-    return(1);
+	if (!chars(arg))
+		return (0);
+	i = 1;
+	while (i < argc)
+	{
+		if (ft_atoi(arg[i]) > 2147483647 || ft_atoi(arg[i]) < -2147483648)
+		{
+			ft_printf("Error\nNúmeros fuera de rango");
+			return (0);
+		}
+		i++;
+	}
+	if (!repeated_numbers(argc, arg))
+		return (0);
+	return (1);
 }
--- push_swap.c
+++ push_swap.c
@@ -6,7 +6,7 @@
 /*   By: issierra <issierra@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/29 11:52:57 by issierra          #+#    #+#             */
-/*   Updated: 2023/12/23 17:58:06 by issierra         ###   ########.fr       */
+/*   Updated: 2023/12/23 18:20:44 by issierra         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -72,66 +72,47 @@ int main(int argc, char *argv[])
     B = NULL;
     //atexit(leaks);
     int i;
+
+    if (argc < 2)
+        return(0);
+    
     if (argc == 2 && argv[1][1] != '\0')
     {
         argv = ft_split(argv[1], ' ');
-        //imprimimos la lista de argumentos
         i = 0;
         while(argv[i])
-        {
-            //ft_printf("argv %i %s", i, argv[i]);
             i++;
-        }
         argc = i;
     }
 
-    if (argc < 2)
-    {
-        //ft_printf("Error.\nNúmero de argumentos inválidos %i\n", argc);
-        return(0);
-    }
+    if (!check_arg(argc, argv))
+        return (0);
     else
     {
-        //CORREGIR PARA QUE FUNCIONE CON ./push_swap "7 9 8" !!!!
-        // ./push_swap --32 Debería dar error
-        if (!check_arg(argc, argv))
-            return (0);
-        else
+        //Creamos el stack A
+        A = create_stack(argv, argc);   
+        if (is_sorted(A))
         {
-            //ft_printf("numeros validos");
-
-            //Creamos el stack A
-            A = create_stack(argv, argc);
-            
-            if (is_sorted(A))
-            {
-                ft_lst_clear(A);
-                return(0);
-            }
-        
-            //ordenamos dos numeros
-            //ft_printf("len:%i", stack_len(A));
-            if (stack_len(A) == 2)
-            {
-                if (A->nbr > A->next->nbr)
-                    sa(&A);
-            }
-            else if(stack_len(A) == 3)
-            {   if (is_sorted(A))
-                    return (0);
-                else 
-                    sort_three(&A);
-            }
-            else
-                sort(&A, &B);
-            
-            //ft_printf("is_sorted:%i", is_sorted(A));
-            //imprimir_lista(A);
             ft_lst_clear(A);
-            //imprimir_lista(A);
-            
+            return(0);
         }
+        if (stack_len(A) == 2)
+        {
+            if (A->nbr > A->next->nbr)
+                sa(&A);
+        }
+        else if(stack_len(A) == 3)
+        {   if (is_sorted(A))
+                return (0);
+            else 
+                sort_three(&A);
+        }
+        else
+            sort(&A, &B);
+        //imprimir_lista(A);
+        ft_lst_clear(A);
+        //imprimir_lista(A);
+        
     }
-    
     return(0);
 }
