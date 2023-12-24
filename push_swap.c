4f874055c3ef70aae22b500e8fd00a4ff8d78b96
Isa Sierra <siemeris@gmail.com>
2023-12-24 11:23:08 +0100

    norminette OK

--- push_swap.c
+++ push_swap.c
@@ -6,7 +6,7 @@
 /*   By: issierra <issierra@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/29 11:52:57 by issierra          #+#    #+#             */
-/*   Updated: 2023/12/24 10:51:22 by issierra         ###   ########.fr       */
+/*   Updated: 2023/12/24 11:22:05 by issierra         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,24 +18,24 @@
 
 //SOLUCIONAR LEAKS CON STRINGS!!!
 
-void leaks()
+void	leaks(void)
 {
-    system("leaks -q push_swap");
+	system("leaks -q push_swap");
 }
 
-void ft_lst_clear(t_stack *lst)
+void	ft_lst_clear(t_stack *lst)
 {
-    t_stack *aux;
+	t_stack	*aux;
 
-    if(!lst)
-        return;
-    while (lst)
-    {
-        aux = lst->next;
-        free(lst);
-        lst = aux;
-    }
-    lst = NULL;
+	if (!lst)
+		return ;
+	while (lst)
+	{
+		aux = lst->next;
+		free(lst);
+		lst = aux;
+	}
+	lst = NULL;
 }
 
 int	sort_cases(t_stack **a)
@@ -62,60 +62,31 @@ int	sort_cases(t_stack **a)
 	return (1);
 }
 
-int main(int argc, char *argv[])
+int	main(int argc, char *argv[])
 {
-    t_stack *A;
-    // t_stack *B;
+	t_stack	*a;
+	int		i;
 
-    A = NULL;
-    // B = NULL;
-    atexit(leaks);
-    int i;
-
-    if (argc < 2)
-        return(0);
-    
-    if (argc == 2 && ft_strchr(argv[1], ' '))
-    {
-        argv = ft_split(argv[1], ' ');
-        i = 0;
-        while(argv[i])
-            i++;
-        argc = i;
-    }
-
-    if (!check_arg(argc, argv))
-        return (0);
-
-        //Creamos el stack A
-        A = create_stack(argv, argc);   
-        if (!sort_cases(&A))
-        {
-            ft_lst_clear(A);
-            return(0);
-        }
-        // if (is_sorted(A))
-        // {
-        //     ft_lst_clear(A);
-        //     return(0);
-        // }
-        // if (stack_len(A) == 2)
-        // {
-        //     if (A->nbr > A->next->nbr)
-        //         sa(&A);
-        // }
-        // else if(stack_len(A) == 3)
-        // {   if (is_sorted(A))
-        //         return (0);
-        //     else 
-        //         sort_three(&A);
-        // }
-        // else
-        //     sort(&A, &B);
-        // //imprimir_lista(A);
-        ft_lst_clear(A);
-        //imprimir_lista(A);
-        
-    
-    return(0);
+	a = NULL;
+	atexit(leaks);
+	if (argc < 2)
+		return (0);
+	if (argc == 2 && ft_strchr(argv[1], ' '))
+	{
+		argv = ft_split(argv[1], ' ');
+		i = 0;
+		while (argv[i])
+			i++;
+		argc = i;
+	}
+	if (!check_arg(argc, argv))
+		return (0);
+	a = create_stack(argv, argc);
+	if (!sort_cases(&a))
+	{
+		ft_lst_clear(a);
+		return (0);
+	}
+	ft_lst_clear(a);
+	return (0);
 }
